d661312a7340ab7bd08f1849f8c1a4266dd11608
Yejin Shin <shinyejin0212@gmail.com>
2022-11-11 19:07:05 +0900

    feat : marker 성공

--- src/components/Address/KakaoMap.js
+++ src/components/Address/KakaoMap.js
@@ -1,33 +1,48 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { marketdata } from "../../data/MarketInfo";
 
-const { kakao } = window;
-
 function KakaoMap() {
-  useEffect(() => {
-    mapscript();
-  }, []);
-  const mapscript = () => {
-    var container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
-    var options = {
-      //지도를 생성할 때 필요한 기본 옵션
-      center: new kakao.maps.LatLng(37.57096169, 126.9984922), // 지도 중심 좌표
-      level: 5, //지도의 레벨(확대, 축소 정도)
+  const { kakao } = window;
+  const [kakaoMap, setKakaoMap] = useState(null);
+  const mapContainer = useRef(null);
+
+  const initMap = () => {
+    const center = new kakao.maps.LatLng(37.57096169, 126.9984922);
+    const options = {
+      center,
+      level: 8,
     };
-
-    const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-
-    for (let i = 0; i < marketdata.length; i++) {
-      let gu = marketdata[i];
-      for (let j = 0; j < 1; j++) {
-        let market = gu.market[j];
-        console.log(market.latitude);
-      }
-    }
+    const map = new kakao.maps.Map(mapContainer.current, options);
+    setKakaoMap(map);
   };
+
+  useEffect(() => {
+    initMap();
+  }, []);
+
+  marketdata.forEach((gu) => {
+    gu.market.forEach((market) => {
+      console.log(market.latitude, market.longitude);
+
+      let marker_position = new kakao.maps.LatLng(
+        market.latitude,
+        market.longitude
+      );
+
+      let marker = new kakao.maps.Marker({
+        position: marker_position,
+        title: market.marekt_name,
+        image: null,
+        clickable: true,
+      });
+      marker.setMap(kakaoMap);
+    });
+  });
+
   return (
     <div
       id="map"
+      ref={mapContainer}
       style={{ width: "349px", height: "298px", borderRadius: "12px" }}
     ></div>
   );
