9bbade148be04507a9ce85abc064eef610eeaf5c
Yejin Shin <shinyejin0212@gmail.com>
2022-11-18 13:57:20 +0900

    feat: 선택 마켓 버튼 추가

--- src/components/Address/BottomSheet.js
+++ src/components/Address/BottomSheet.js
@@ -9,12 +9,18 @@ const { kakao } = window;
 
 function BottomSheet(open) {
   const currentLocation = useGeoLocation();
-  const navigate = useNavigate();
   const [flag, setflag] = useState(!open);
+  const [market, setMarket] = useState(null);
+
   const onClickCurrent = () => {
     setflag(1);
   };
-  return flag == 0 ? (
+
+  const onClickChoice = (choice_market) => {
+    setMarket(choice_market);
+  };
+
+  return flag == 1 ? ( //0으로 바꿔야함
     <>
       <div>주소설정</div>
       <Button onClick={onClickCurrent}>현재 위치로 설정</Button>
@@ -24,6 +30,8 @@ function BottomSheet(open) {
   ) : (
     <>
       <KakaoMap location={currentLocation} />
+
+      <Button onClick={onClickChoice}>선택</Button>
     </>
   );
 }
